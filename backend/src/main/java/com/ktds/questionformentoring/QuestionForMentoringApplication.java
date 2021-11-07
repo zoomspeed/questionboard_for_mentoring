ee29bd8fd3ad5bc6fcc3ccb71fc8bb3a6403292b
윤경호 <zoomspeed77@gmail.com>
2021-11-07 22:41:34 +0900

    [Add] init commit

---
+++ backend/src/main/java/com/ktds/questionformentoring/QuestionForMentoringApplication.java
@@ -0,0 +1,13 @@
+package com.ktds.questionformentoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionForMentoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionForMentoringApplication.class, args);
+	}
+
+}
