0a93759414113d1b78bee7bd790f32346ccd6c41
nueli <asdfz888@naver.com>
2021-12-06 15:08:26 +0900

    [Add]: BootstrapVue 헤더 추가

--- backend/src/frontend/src/views/Home.vue
+++ backend/src/frontend/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Header />
     <MainMenu />
     <HelloWorld />
   </div>
@@ -7,12 +8,14 @@
 
 <script>
 // @ is an alias to /src
+import Header from '@/components/layout/Header.vue'
 import MainMenu from '@/components/MainMenu.vue'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
+    Header,
     MainMenu,
     HelloWorld
   }
