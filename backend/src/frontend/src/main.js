1fbca6799d9416f21832aa110b691f43ab2bbc4f
nueli <asdfz888@naver.com>
2021-12-06 14:03:35 +0900

    [Add]: BootstrapVue main.js 수정

--- backend/src/frontend/src/main.js
+++ backend/src/frontend/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// BootstrapVue 추가
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// BootstrapVue 사용
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 new Vue({
