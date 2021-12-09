de748572d1d803843a8922707568555ba7d38563
chaeyeonkim0223 <tomybaru@naver.com>
2021-12-09 16:22:31 +0900

    [Fix]: Bootstrap 주석

--- backend/src/frontend/src/main.js
+++ backend/src/frontend/src/main.js
@@ -4,11 +4,11 @@ import router from './router'
 import store from './store'
 
 // BootstrapVue 추가
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import BootstrapVue from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 // BootstrapVue 사용
-Vue.use(BootstrapVue)
+// Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
