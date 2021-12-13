6f131fd74279e3895f06d6907e92d028ced2f69b
chaeyeonkim0223 <tomybaru@naver.com>
2021-12-13 10:55:51 +0900

    [Feat] : Comment package 주석

--- backend/src/main/java/com/ktds/questionformentoring/comment/service/CommentServiceImpl.java
+++ backend/src/main/java/com/ktds/questionformentoring/comment/service/CommentServiceImpl.java
@@ -1,17 +1,18 @@
-package com.ktds.questionformentoring.comment.service;
-
-import com.ktds.questionformentoring.comment.mapper.CommentMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CommentServiceImpl implements CommentService {
-
-    @Autowired
-    private CommentMapper boardMapper;
-
-    @Override
-    public String testSelect() {
-        return boardMapper.testSelect();
-    }
-}
+//package com.ktds.questionformentoring.comment.service;
+//
+//import com.ktds.questionformentoring.board.entity.BoardVo;
+//import com.ktds.questionformentoring.comment.mapper.CommentMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CommentServiceImpl implements CommentService {
+//
+//    @Autowired
+//    private CommentMapper boardMapper;
+//
+//    @Override
+//    public BoardVo testSelect() {
+//        return boardMapper.testSelect();
+//    }
+//}
