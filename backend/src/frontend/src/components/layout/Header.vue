84f54667c9a70e98dd22366c816f58a7f8734751
윤경호 <zoomspeed77@gmail.com>
2021-12-31 02:26:02 +0900

    젠킨스 배포 테스트

--- backend/src/frontend/src/components/layout/Header.vue
+++ backend/src/frontend/src/components/layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">게시판</b-navbar-brand>
+      <b-navbar-brand href="#">게시판_jenkins 배포 테스트</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
